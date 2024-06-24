1592b29616cb0e799b693e642796441e8a304dd3
manishc22 <100667319+manishc22@users.noreply.github.com>
2024-06-24 16:55:51 +0530

    V38

--- pages/9_Payment_Analysis.py
+++ pages/9_Payment_Analysis.py
@@ -1,6 +1,19 @@
 import streamlit.components.v1 as components
 import streamlit as st
 
+st.set_page_config(page_title="Payment Analysis",
+                   layout='wide', initial_sidebar_state='collapsed')
+
+st.markdown(
+    """
+<style>
+    [data-testid="collapsedControl"] {
+        display: none
+    }
+</style>
+""",
+    unsafe_allow_html=True,
+)
 with st.expander(label='Overview'):
     components.iframe('https://embed.deepnote.com/8fb5748f-8fc7-4d4a-9fec-9ea603ee251a/14e830f941614c8491f92448466b0629/f07cae534207473eb20cfc887d170970?height=441', height=600,
                       width=None, scrolling=False)
